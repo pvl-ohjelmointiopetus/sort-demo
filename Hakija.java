9fbc99d6643156de37bed982af80d9c5c86bd99b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-07-12 20:34:57 +0300

    Runkoa

---
+++ Hakija.java
@@ -0,0 +1,15 @@
+/****
+ *  Koodi lukee tiedoston ja kertoo, onko annettu sana siina tiedostossa.
+ **/
+
+import java.util.Scanner;
+
+public class Hakija {
+
+	/*** Tahan luetaan kayttajan syote nappikselta. */
+	private static Scanner nappis = new Scanner(System.in);
+
+    /*** Tahan juttuun luetaan tiedosto */
+	private static Scanner tiedos = null;
+
+}
